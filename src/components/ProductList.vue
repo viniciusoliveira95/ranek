ffa9a9408e14736671ca4003e83b7aeeb71e7862
viniciusoliveira95 <vinicius_goliveira@hotmail.com>
2020-12-04 14:13:17 -0300

    feature/add axios

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,10 +1,38 @@
 <template>
-  <p>Lista de produtos</p>
+  <section class="produtcts-container">
+    <div v-for="product in products" :key="product.id">
+      <img
+        v-if="product.photos"
+        :src="product.photos[0].src"
+        alt="product.photos[0].title"
+      />
+      <p class="price">{{ product.price }}</p>
+      <h2 class="title">{{ product.name }}</h2>
+      <p>{{ product.description }}</p>
+    </div>
+  </section>
 </template>
 
 <script>
+import api from "./../services/api.js";
+
 export default {
   name: "ProductList",
+  data() {
+    return {
+      products: null,
+    };
+  },
+
+  methods: {
+    getProducts() {
+      api.get("/product").then((response) => (this.products = response.data));
+    },
+  },
+
+  created() {
+    this.getProducts();
+  },
 };
 </script>
 
