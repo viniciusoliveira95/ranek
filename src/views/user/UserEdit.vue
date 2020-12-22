c6f7838533e6132152baa4552ae30616a0a70455
viniciusoliveira95 <vinicius_goliveira@hotmail.com>
2020-12-22 14:26:38 -0300

    feature/update user and delete product

--- src/views/user/UserEdit.vue
+++ src/views/user/UserEdit.vue
@@ -1,10 +1,30 @@
 <template>
-  <p>Editar Usuário</p>
+  <section>
+    <UserForm>
+      <button class="btn" @click.prevent="updateUser">Atualizar Usuário</button>
+    </UserForm>
+  </section>
 </template>
 
 <script>
+import UserForm from "./../../components/UserForm";
+import api from "./../../services/api";
+
 export default {
   name: "UserEdit",
+  components: { UserForm },
+
+  methods: {
+    updateUser() {
+      api
+        .put(`user/${this.$store.state.user.id}`, this.$store.state.user)
+        .then(() => {
+          this.$store.dispatch("getUser");
+          this.$router.push({ name: "user" });
+        })
+        .catch((error) => console.log(error));
+    },
+  },
 };
 </script>
 
--- src/views/user/UserProducts.vue
+++ src/views/user/UserProducts.vue
@@ -7,6 +7,9 @@
       <li v-for="(product, index) in userProducts" :key="index">
         <ProductItem :product="product">
           <p>{{ product.description }}</p>
+          <button class="delete" @click="deleteProduct(product.id)">
+            Deletar
+          </button>
         </ProductItem>
       </li>
     </transition-group>
@@ -18,6 +21,7 @@ import { mapState, mapActions } from "vuex";
 
 import AddProduct from "./../../components/AddProduct";
 import ProductItem from "./../../components/ProductItem";
+import api from "./../../services/api";
 
 export default {
   name: "UserProducts",
@@ -28,6 +32,17 @@ export default {
 
   methods: {
     ...mapActions(["getUserProducts"]),
+
+    deleteProduct(id) {
+      const confirm = window.confirm("Deseja remover este produto?");
+
+      if (confirm) {
+        api
+          .delete(`product/${id}`)
+          .then(() => this.getUserProducts())
+          .catch((error) => console.log(error));
+      }
+    },
   },
 
   watch: {
@@ -58,5 +73,18 @@ h2 {
 .list-leave-to {
   opacity: 0;
   transform: translate3d(20px, 0, 0);
+}
+
+.delete {
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  background: url("../../assets/remove.svg") no-repeat center center;
+  width: 24px;
+  height: 24px;
+  text-indent: -140px;
+  overflow: hidden;
+  cursor: pointer;
+  border: none;
 }
 </style>
